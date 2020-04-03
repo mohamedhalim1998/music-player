cdbc012781f4577825f4fdf3e83d029e8eb78b04
Mohamed Halim <mohamedhalim1998@gmail.com>
2020-04-03 03:02:55 +0200

    edit the design

--- app/src/main/java/com/mohamed/halim/essa/mymusic/adapters/AudioAdapter.java
+++ app/src/main/java/com/mohamed/halim/essa/mymusic/adapters/AudioAdapter.java
@@ -12,6 +12,10 @@ import android.widget.TextView;
 
 import com.mohamed.halim.essa.mymusic.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class AudioAdapter extends BaseAdapter {
     private Context context;
     private Cursor audioCursor;
@@ -55,22 +59,38 @@ public class AudioAdapter extends BaseAdapter {
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.audio_list_item, parent, false);
         }
-        int idColumn = audioCursor.getColumnIndex(MediaStore.Audio.Media._ID);
         int titleColumn = audioCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
         int artistColumn = audioCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
         int albumColumn = audioCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM);
         int albumIdColumn = audioCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID);
         int dataAddedColumn = audioCursor.getColumnIndex(MediaStore.Audio.Media.DATE_ADDED);
-
+        int durationColumn = audioCursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
         audioCursor.moveToPosition(position);
-        int id = audioCursor.getInt(idColumn);
         String title = audioCursor.getString(titleColumn);
         String artist = audioCursor.getString(artistColumn);
         String album = audioCursor.getString(albumColumn);
         int albumId = audioCursor.getInt(albumIdColumn);
         long dataAdded = audioCursor.getLong(dataAddedColumn);
+        long duration = audioCursor.getLong(durationColumn);
         TextView titleTextView = convertView.findViewById(R.id.title_tv);
+        TextView artistTextView = convertView.findViewById(R.id.album_artist_tv);
+        TextView durationTextView = convertView.findViewById(R.id.duration_tv);
         titleTextView.setText(title);
+        artistTextView.setText(String.format("%s - %s", artist, album));
+        durationTextView.setText(formatDuration(duration));
         return convertView;
+    }
+
+    private String formatDuration(long duration) {
+        long seconds = duration / 1000;
+        long min = seconds / 60;
+        seconds = seconds - min * 60;
+        long hours = min / 60;
+        min = min - hours * 60;
+        if (hours != 0)
+            return String.format(Locale.getDefault(), "%d:%02d:%02d", hours, min, seconds);
+        else
+            return String.format(Locale.getDefault(), "%02d:%02d", min, seconds);
+
     }
 }
